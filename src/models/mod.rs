63bec6cbd2152ec432eaca9fe7c43887638a89ea
vnxfsc <1047658287@qq.com>
2025-05-13 08:48:18 +0800

    feat: 更新event事件结构

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -11,6 +11,10 @@ pub struct CreateEvent {
     pub user: Pubkey,
     pub creator: Pubkey,
     pub timestamp: i64,
+    pub virtual_token_reserves: u64,
+    pub virtual_sol_reserves: u64,
+    pub real_token_reserves: u64,
+    pub token_total_supply: u64,
 }
 
 #[derive(Clone, Debug, Default, PartialEq, BorshDeserialize, BorshSerialize)]
@@ -28,11 +32,17 @@ pub struct TradeEvent {
     pub token_amount: u64,
     pub is_buy: bool,
     pub user: Pubkey,
-    pub timestamp: u64,
+    pub timestamp: i64,
     pub virtual_sol_reserves: u64,
     pub virtual_token_reserves: u64,
     pub real_sol_reserves: u64,
     pub real_token_reserves: u64,
+    pub fee_recipient: Pubkey,
+    pub fee_basis_points: u64,
+    pub fee: u64,
+    pub creator: Pubkey,
+    pub creator_fee_basis_points: u64,
+    pub creator_fee: u64,
 }
 
 #[derive(Clone, Debug, Default, PartialEq, BorshDeserialize, BorshSerialize)]
@@ -57,29 +67,9 @@ pub struct BuyEvent {
     pub user_quote_token_account: Pubkey,
     pub protocol_fee_recipient: Pubkey,
     pub protocol_fee_recipient_token_account: Pubkey,
-}
-
-#[derive(Clone, Debug, Default, PartialEq, BorshDeserialize, BorshSerialize)]
-pub struct CreatePoolEvent {
-    pub timestamp: i64,
-    pub index: u16,
-    pub creator: Pubkey,
-    pub base_mint: Pubkey,
-    pub quote_mint: Pubkey,
-    pub base_mint_decimals: u8,
-    pub quote_mint_decimals: u8,
-    pub base_amount_in: u64,
-    pub quote_amount_in: u64,
-    pub pool_base_amount: u64,
-    pub pool_quote_amount: u64,
-    pub minimum_liquidity: u64,
-    pub initial_liquidity: u64,
-    pub lp_token_amount_out: u64,
-    pub pool_bump: u8,
-    pub pool: Pubkey,
-    pub lp_mint: Pubkey,
-    pub user_base_token_account: Pubkey,
-    pub user_quote_token_account: Pubkey,
+    pub coin_creator: Pubkey,
+    pub coin_creator_fee_basis_points: u64,
+    pub coin_creator_fee: u64,
 }
 
 #[derive(Clone, Debug, Default, PartialEq, BorshDeserialize, BorshSerialize)]
@@ -104,4 +94,31 @@ pub struct SellEvent {
     pub user_quote_token_account: Pubkey,
     pub protocol_fee_recipient: Pubkey,
     pub protocol_fee_recipient_token_account: Pubkey,
+    pub coin_creator: Pubkey,
+    pub coin_creator_fee_basis_points: u64,
+    pub coin_creator_fee: u64,
+}
+
+#[derive(Clone, Debug, Default, PartialEq, BorshDeserialize, BorshSerialize)]
+pub struct CreatePoolEvent {
+    pub timestamp: i64,
+    pub index: u16,
+    pub creator: Pubkey,
+    pub base_mint: Pubkey,
+    pub quote_mint: Pubkey,
+    pub base_mint_decimals: u8,
+    pub quote_mint_decimals: u8,
+    pub base_amount_in: u64,
+    pub quote_amount_in: u64,
+    pub pool_base_amount: u64,
+    pub pool_quote_amount: u64,
+    pub minimum_liquidity: u64,
+    pub initial_liquidity: u64,
+    pub lp_token_amount_out: u64,
+    pub pool_bump: u8,
+    pub pool: Pubkey,
+    pub lp_mint: Pubkey,
+    pub user_base_token_account: Pubkey,
+    pub user_quote_token_account: Pubkey,
+    pub coin_creator: Pubkey,
 } 
